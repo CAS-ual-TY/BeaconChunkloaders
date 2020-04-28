80fd9fdf74d8c68ab758ae56e3880a22e77dfdf2
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-04-28 16:58:17 +0200

    Update main mod class

--- src/main/java/de/cas_ual_ty/beaconchunkloaders/BeaconChunkloaders.java
+++ src/main/java/de/cas_ual_ty/beaconchunkloaders/BeaconChunkloaders.java
@@ -5,9 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Rarity;
 import net.minecraft.nbt.INBT;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
@@ -20,6 +22,7 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -31,26 +34,25 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BeaconChunkloaders
 {
     public static final String MODID = "beaconchunkloaders";
-    public static final String LOADERID = "loader";
+    public static final String BEACON_ID = "beacon";
     
-    @SuppressWarnings("unused")
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
     
     @CapabilityInject(IChunkLoaderList.class)
     public static Capability<IChunkLoaderList> CAPABILITY = null;
     
-    private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, BeaconChunkloaders.MODID);
-    private static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, BeaconChunkloaders.MODID);
+    private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, "minecraft");
+    private static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, "minecraft");
     
-    public static final RegistryObject<Block> LOADER_BLOCK = BeaconChunkloaders.BLOCKS.register(BeaconChunkloaders.LOADERID, () -> new LoaderBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.5F)));
-    public static final RegistryObject<Item> LOADER_ITEM = BeaconChunkloaders.ITEMS.register(BeaconChunkloaders.LOADERID, () -> new BlockItem(BeaconChunkloaders.LOADER_BLOCK.get(), new Item.Properties().group(ItemGroup.MISC)));
+    public static final RegistryObject<Block> NEW_BEACON_BLOCK = BeaconChunkloaders.BLOCKS.register(BeaconChunkloaders.BEACON_ID, () -> new LoaderBlock(Block.Properties.create(Material.GLASS, MaterialColor.DIAMOND).hardnessAndResistance(3.0F).lightValue(15).notSolid()));
+    public static final RegistryObject<Item> NEW_BEACON_ITEM = BeaconChunkloaders.ITEMS.register(BeaconChunkloaders.BEACON_ID, () -> new BlockItem(BeaconChunkloaders.NEW_BEACON_BLOCK.get(), (new Item.Properties()).group(ItemGroup.MISC).rarity(Rarity.RARE)));
     
     public BeaconChunkloaders()
     {
         BeaconChunkloaders.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         BeaconChunkloaders.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        MinecraftForge.EVENT_BUS.addListener(this::attachWorldCaps);
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
     private void setup(final FMLCommonSetupEvent event)
@@ -58,9 +60,10 @@ public class BeaconChunkloaders
         CapabilityManager.INSTANCE.register(IChunkLoaderList.class, new ChunkLoaderList.Storage(), () -> new ChunkLoaderList(null));
     }
     
-    private void attachWorldCaps(AttachCapabilitiesEvent<World> event)
+    @SubscribeEvent
+    public void attachWorldCaps(AttachCapabilitiesEvent<World> event)
     {
-        if(event.getObject().isRemote)
+        if(!(event.getObject() instanceof ServerWorld))
         {
             return;
         }
@@ -85,7 +88,7 @@ public class BeaconChunkloaders
                 BeaconChunkloaders.CAPABILITY.readNBT(inst.orElse(null), null, nbt);
             }
         };
-        event.addCapability(new ResourceLocation(BeaconChunkloaders.MODID, BeaconChunkloaders.LOADERID), provider);
+        event.addCapability(new ResourceLocation(BeaconChunkloaders.MODID, BeaconChunkloaders.BEACON_ID), provider);
         event.addListener(() -> inst.invalidate());
     }
 }
